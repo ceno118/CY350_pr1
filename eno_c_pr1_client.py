fe560dd1948b92e4f3b8821bf6b80658e7432a45
ceno118 <ceno118@gmail.com>
2023-02-07 16:08:58 -0500

    basic c/s works

--- eno_c_pr1_client.py
+++ eno_c_pr1_client.py
@@ -1,6 +1,17 @@
 import socket
 import time
 import random
-import sys
 
-print("hello")
+serverName = input("Enter server name: ")
+serverPort = int(input("Enter server port: "))
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+clientSocket.connect((serverName, serverPort))
+
+for i in range(1,11):
+    message = 'ping'
+    clientSocket.send(message.encode())
+    modifiedMessage = clientSocket.recv(1024).decode()
+    print('From Server: ', modifiedMessage)
+    clientSocket.close()
--- eno_c_pr1_server.py
+++ eno_c_pr1_server.py
@@ -1,6 +1,21 @@
 import socket
 import time
 import random
-import sys
 
 print("hello")
+
+serverPort = 12000
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+
+print("The server is ready to receive")
+
+# he doesn't want more than 30% packet loss, so make it random unless 3 have already been dropped
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    print("Message from: ", clientAddress, ": ", message.decode())
+    modifiedMessage = message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+
