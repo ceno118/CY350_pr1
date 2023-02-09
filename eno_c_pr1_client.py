b83048f458304284ef6b4234faa1be26372032f4
Christopher Eno <ceno118@gmail.com>
2023-02-09 00:01:49 -0500

    code done

--- eno_c_pr1_client.py
+++ eno_c_pr1_client.py
@@ -1,17 +1,77 @@
+# MY ONLY REFERENCES USED WERE PYTHON DOCUMENTATION AND CLASS NOTES WHICH ARE COMMON KNOWLEDGE
+
 import socket
 import time
-import random
 
-serverName = input("Enter server name: ")
+serverName = input("Enter server name or IP: ")
 serverPort = int(input("Enter server port: "))
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 clientSocket.connect((serverName, serverPort))
+clientSocket.settimeout(1)
+
+def getMonth(mon):
+    if mon == 1:
+        return "Jan"
+    elif mon == 2:
+        return "Feb"
+    elif mon == 3:
+        return "Mar"
+    elif mon == 4:
+        return "Apr"
+    elif mon == 5:
+        return "May"
+    elif mon == 6:
+        return "Jun"
+    elif mon == 7:
+        return "Jul"
+    elif mon == 8:
+        return "Aug"
+    elif mon == 9:
+        return "Sep"
+    elif mon == 10:
+        return "Oct"
+    elif mon == 11:
+        return "Nov"
+    else:
+        return "Dec"
+
+msg = input("What message would you like to send?: ") #should be "ping", but other inputs demonstrate server's error checking
+
+sent = 0
+received = 0
+dropped = 0
+RTTs = []
 
 for i in range(1,11):
-    message = 'ping'
+    seq_number = i
+    cur_time = time.localtime()
+    month = cur_time.tm_mon
+    timestring = str(cur_time.tm_mday) + " " + str(cur_time.tm_hour)+":"+str(cur_time.tm_min)+":"+str(cur_time.tm_sec)+" " + getMonth(month) + " " + str(cur_time.tm_year)
+    message = msg + " " + str(seq_number) + " " + timestring
     clientSocket.send(message.encode())
-    modifiedMessage = clientSocket.recv(1024).decode()
-    print('From Server: ', modifiedMessage)
-    clientSocket.close()
+    sent += 1
+    send_time = time.time()
+    try:
+        modifiedMessage = clientSocket.recv(1024).decode()
+        rtt = (time.time() - send_time) * 1000 # converts time to milliseconds
+        RTTs.append(rtt)
+        if modifiedMessage[:8] == "pong "+ message[5:8]: #makes sure the response message and seq number are correct
+            print('From Server: ' + modifiedMessage + " RTT: " + str(rtt) + " seconds")
+        else:
+            print("Incorrect response from server: ", modifiedMessage)
+        received += 1
+        
+    except:
+        print("No response received for ping " + str(seq_number))
+        dropped += 1
+
+clientSocket.close()
+print("")
+print("Packets Sent: " + str(sent))
+print("Packets Received: " + str(received))
+print("Success Rate: " + str(int((received/sent) * 100)) + "%")
+print("Minimum RTT: " + str(min(RTTs)) + " milliseconds")
+print("Maximum RTT: " + str(max(RTTs)) + " milliseconds")
+print("Average RTT: " + str(sum(RTTs) / len(RTTs)) + " milliseconds")
+print("")
--- eno_c_pr1_server.py
+++ eno_c_pr1_server.py
@@ -1,10 +1,9 @@
+# MY ONLY REFERENCES USED WERE PYTHON DOCUMENTATION AND CLASS NOTES WHICH ARE COMMON KNOWLEDGE
+
 import socket
-import time
 import random
 
-print("hello")
-
-serverPort = 12000
+serverPort = int(input("Enter server port: "))
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 
@@ -12,10 +11,32 @@ print("The server is ready to receive")
 
 # he doesn't want more than 30% packet loss, so make it random unless 3 have already been dropped
 
+dropped = 0
+
+def decideLoss(): # this simulates up to 30% packet loss
+    check = random.choice(range(100))
+    if check < 30:
+        global dropped
+        dropped += 1
+        return True
+    else:
+        return False
+
+
 while True:
+
     message, clientAddress = serverSocket.recvfrom(2048)
-    print("Message from: ", clientAddress, ": ", message.decode())
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    decoded = message.decode()
+    print("Message from: ", clientAddress, ": ", decoded)
+
+    if dropped < 3 and decideLoss(): # checks if we've hit 30% loss
+        print("drop") # not necessary, but helps the user see when things are dropped
+
+    elif decoded[:4] == "ping":
+        modifiedMessage = "pong" + message[4:].decode()
+        serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    
+    else:
+        dropped += 1
 
 
